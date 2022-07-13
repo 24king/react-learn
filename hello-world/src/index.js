f10b1ce4025fe07da085459a7e11c27837c832db
limix <coollimix@gmail.com>
2022-07-13 23:05:15 +0800

    状态全部保存在父组件当中

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -12,18 +12,28 @@ class Square extends React.Component {
   render() {
     return (
       // 注意:箭头函数
-      <button className="square" onClick={() => this.setState({
-        value:'X'
-      })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
   
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X'
+    this.setState({squares:squares})
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
